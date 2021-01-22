082a6129deef9d570cd6ee05c37f398ba67dc100
vishnukiran51 <vishnukiran90@gmail.com>
2021-01-22 21:56:47 +0100

    introduced an example of serialization in java

--- src/corejava/serializing.java
+++ src/corejava/serializing.java
@@ -1,4 +1,30 @@
-package corejava;/**
+package corejava;
+
+import java.io.*;
+import java.sql.Savepoint;
+
+/**
+ * Simple Serialization in java
  * @author Vishnu Kandanelly
- */public class serializing {
+ */
+public class serializing {
+    public static void main(String[] args) throws Exception {
+        Save obj = new Save();
+        obj.i = 1;
+
+        File file = new File("obj.txt");
+        FileOutputStream fileOutputStream = new FileOutputStream(file);
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+        objectOutputStream.writeObject(objectOutputStream);//TODO: resolve the exception
+
+        FileInputStream fileInputStream = new FileInputStream(file);
+        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+        Save objNew = (Save) objectInputStream.readObject();
+
+        System.out.println(objNew.i);
+    }
+}
+
+class Save implements Serializable {
+    int i;
 }
