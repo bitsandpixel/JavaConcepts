1324849e7e527a9a79d9dfb6b7e565b7f543d9d9
vishnukiran51 <vishnukiran90@gmail.com>
2021-02-10 23:56:58 +0100

    added functional interface

--- src/advancedjava/java8features/FunctionalInterface.java
+++ src/advancedjava/java8features/FunctionalInterface.java
@@ -1,9 +1,29 @@
 package advancedjava.java8features;
 
 public class FunctionalInterface {
-
+    public static void main(String[] args) {
+        Phone phone = new AndriodPhone();
+        phone.call();
+        phone.message();
+    }
 }
 
-interface Phone{
+interface Phone {
+    //TODO: variables in interfaces
+    void call();/*every method in interfaces are abstract and public so we can omit them while declaring
+    */
 
+    /*now methods can have definition in interface with default keyword */
+    default void message() {
+        System.out.println("message");
+    }
+}
+
+
+class AndriodPhone implements Phone {
+
+    @Override
+    public void call() {
+        System.out.println("Calling");
+    }
 }
