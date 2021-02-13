b17cb1327eee15ba7703982df4217ddffd41dda6
vishnukiran51 <vishnukiran90@gmail.com>
2021-02-13 15:08:32 +0100

    Added minimal changes to arraylist, method references demo files

--- src/advancedjava/java8features/MethodReferenceDemo.java
+++ src/advancedjava/java8features/MethodReferenceDemo.java
@@ -9,6 +9,10 @@ import java.util.List;
  * It is compact and easy form of lambda expression.
  * Each time when you are using lambda expression to just referring a method,
  * you can replace your lambda expression with method reference.
+ * <p>
+ * There are certain rules in order to use Method reference as an alternative to lambda expressions.
+ * -- 1) When you have a lambda expression, which takes in no input argument and its calling a method without
+ * any argument then we can replace that lambda expression with a method reference.
  */
 public class MethodReferenceDemo {
     public static void main(String[] args) {
@@ -19,5 +23,13 @@ public class MethodReferenceDemo {
         List<String> cities = Arrays.asList("Rome", "Cairo", "Calcutta");
 //        cities.forEach(city -> System.out.println(city)); //using lambda expression
         cities.forEach(System.out::println);
+
+//        Thread thread = new Thread(() -> printMessage());
+        Thread thread = new Thread(MethodReferenceDemo::printMessage);
+        thread.start();
+    }
+
+    private static void printMessage() {
+        System.out.println("Hello World!");
     }
 }
