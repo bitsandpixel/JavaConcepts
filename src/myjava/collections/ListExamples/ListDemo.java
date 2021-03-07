1cf46b016d30d53f2b85d8a6628b11dcc62b9ed6
vishnukiran51 <vishnukiran90@gmail.com>
2021-03-07 23:56:59 +0100

    added list demo class

---
+++ src/myjava/collections/ListExamples/ListDemo.java
@@ -0,0 +1,30 @@
+package myjava.collections.ListExamples;
+
+/**
+ * @author Vishnu Kandanelly
+ */
+public class ListDemo {
+}
+
+class PersonList{
+    private PersonElement personElements;
+}
+
+class PersonElement{
+    /**
+     * The object that contains the exam information
+     */
+    private Person person;
+    /**
+     * The next element in this list
+     */
+    private PersonElement next;
+}
+
+
+
+class Person{
+    String firstName;
+    String lastName;
+    int age;
+}
