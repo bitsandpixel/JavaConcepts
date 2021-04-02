e84749c65e5e0fffc8aeff12a312b3d237cda6bf
vishnukiran51 <vishnukiran90@gmail.com>
2021-04-02 22:48:29 +0200

    random commit

--- src/advancedjava/java8features/Streams/StreamFilterCollectionDemo.java
+++ src/advancedjava/java8features/Streams/StreamFilterCollectionDemo.java
@@ -1,5 +1,13 @@
 package advancedjava.java8features.Streams;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * @author Vishnu Kandanelly
  */
@@ -7,5 +15,33 @@ public class StreamFilterCollectionDemo {
 
     public static void main(String[] args) {
 
+        List<Product> productsList = new LinkedList<>();
+        productsList.add(new Product(1, "HP Laptop", 25000f));
+        productsList.add(new Product(2, "Dell Laptop", 30000f));
+        productsList.add(new Product(3, "Lenevo Laptop", 28000f));
+        productsList.add(new Product(4, "Sony Laptop", 28000f));
+        productsList.add(new Product(5, "Apple Laptop", 90000f));
+
+        List<Float> productPriceList = new ArrayList<Float>();
+
+        for (Product product : productsList) {
+            if (product.price < 30000) {
+                productPriceList.add(product.price);
+            }
+        }
+
+        System.out.println(productPriceList);
+
     }
 }
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Product {
+    int id;
+    String name;
+    float price;
+}
+
+
