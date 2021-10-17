bc96379ea3842bab4d9d2efbf1d404d626ba433c
vishnukiran51 <vishnukiran90@gmail.com>
2021-10-17 20:11:28 +0200

    adding playgroud package with sorting example

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+S
+#.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
+}
+Msrc/main/java/basicjava/languagebasics/controlflowstatements/forLoopDemo.java,a\f\afa6dbbc63da0d7e15256f4a314ec3ad2230a53c
+w
+Gsrc/main/java/advancedjava/java8features/streams/MapAndCollectDemo.java,c\e\cedf027adb1aa576f9cb34c2dd2748c31a7e6e80
---
+++ src/main/java/basicjava/playground/SortByColumn.java
@@ -0,0 +1,34 @@
+package basicjava.playground;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class SortByColumn {
+    public static void main(String[] args) throws IOException {
+        /*
+         * Sorting by a particular column
+         */
+        SortedMap<Integer, List<String>> lines = Files.readAllLines(Paths.get("ListOfNames.csv"))
+                .stream().skip(1).distinct().collect(Collectors.groupingBy(
+                        list -> Integer.valueOf(list.split(",", 3)[2]),
+                        TreeMap::new, Collectors.toList()));
+
+        for (Map.Entry<Integer, List<String>> entry : lines.entrySet()) {
+            System.out.println(entry.getValue());
+        }
+
+        /*Sorting in natural order*/
+
+        /*List<String> lines = Files.readAllLines(Paths.get("ListOfNames.csv"));
+        lines = lines.stream().skip(1)
+                .distinct()
+                .collect(Collectors.toList());
+        lines.forEach(line -> Arrays.stream(line.split(",")).sorted(Comparator.naturalOrder())
+                .collect(Collectors.toList())
+                .forEach(System.out::println));*/
+    }
+}
+
