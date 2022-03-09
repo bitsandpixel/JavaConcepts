f4c7e9ce55b6a2c45bce906747d8c1e2eb72434f
vishnukiran51 <vishnukiran90@gmail.com>
2022-03-09 08:26:38 +0100

    added dev comments to Optional Class

--- src/main/java/advancedjava/java8features/optionalclass/OptionalClassDemo.java
+++ src/main/java/advancedjava/java8features/optionalclass/OptionalClassDemo.java
@@ -6,6 +6,7 @@ import java.util.Optional;
  * Tired of using NullPointerExceptions, use "Optional" class
  * @URL https://www.oracle.com/technical-resources/articles/java/java8-optional.html
  * @URL https://www.baeldung.com/java-optional
+ * @URL 'https://youtu.be/1xCxoOuDZuU'
  *
  * @author Vishnu Kandanelly
  */
@@ -29,11 +30,18 @@ public class OptionalClassDemo {
          */
         System.out.println("is present:" + ofOptional.isPresent());
 
+        Optional<String> ofNullable = Optional.ofNullable(null);
         /*
         If we are expecting nullable values, we can also create the Optional object
         with ofNullable().
         By doing this, if we pass in a null reference, it doesn't throw an exception
         but rather returns an empty Optional object
          */
+        System.out.println(ofNullable.isEmpty());//true
+
+        Optional<String> names = Optional.ofNullable("Vishnu");
+        String orElse = names.orElse("Kiran");
+        /*If Value available "Vishnu" would be printed otherwise Kiran would be printed.*/
+        System.out.println(orElse);
     }
 }
